41c06c8ba2a31cb73b66b2263e69740aa4b75460
Leonardo Casagrande <leonardo.casagrande@hitechnologies.com.br>
2021-12-12 13:11:22 -0300

    Made git optional, and edited package json to match the project name

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const schema = {
   properties: {
     projectName: {
       required: true,
+      description: "Enter the project name"
     },
     gitUrl: {
-      required: true,
+      description: "Enter the git url of the project (optional)"
     },
   },
 };
@@ -17,7 +18,6 @@ function onErr(err) {
   console.log(err);
   return 1;
 }
-
 
 prompt.start();
 
@@ -29,26 +29,37 @@ prompt.get(schema, (err, result) => {
   const projectPath = `${__dirname}/${result.projectName}`;
   const git = result.gitUrl;
 
-
   try {
-
     execSync(
       `git clone git@github.com:leonardocasagrande/react-material-boilerplate.git ${result.projectName}`,
       { cwd: __dirname }
-    )
+    );
 
-    execSync('rm -rf .git', { cwd: projectPath })
-    execSync('git init', { cwd: projectPath })
-    execSync(`git remote add origin ${git}`, { cwd: projectPath })
-    execSync('git add .', { cwd: projectPath })
-    execSync('git commit -m "Initial commit" ', { cwd: projectPath })
-    execSync('git push -u origin master', { cwd: projectPath })
-    execSync('npm install', { cwd: projectPath })
-    execSync('npm start', { cwd: projectPath })
+    execSync("rm -rf .git", { cwd: projectPath });
+    const fs = require('fs');
+    console.log("Updating package.json");
+    if (!fs.existsSync(`${projectPath}/package.json`) ) {
+       console.log('file doesnt exists')
+    }
+    else {
+      const content = fs.readFileSync(`${projectPath}/package.json`, 'utf8')
+      const pkg = JSON.parse(content);
+      pkg.name = result.projectName;
+      console.log("PKG", pkg)
+      fs.writeFileSync(`${projectPath}/package.json`, JSON.stringify(pkg, null, "\t"));
+    }
     
+    if (git) {
+      execSync("git init", { cwd: projectPath });
+      execSync(`git remote add origin ${git}`, { cwd: projectPath });
+      execSync("git add .", { cwd: projectPath });
+      execSync('git commit -m "Initial commit" ', { cwd: projectPath });
+      execSync("git push -u origin master", { cwd: projectPath });
+    }
+    console.log("\n\n\nProject created successfully!");
+    console.log(`Run cd ${result.projectName} to start coding!`);
   } catch (err) {
     console.log(`\n  ERROR CLONING PROJECT \n`);
     console.log(err);
   }
-
 });
